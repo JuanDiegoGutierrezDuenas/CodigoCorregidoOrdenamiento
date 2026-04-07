446818d4b756f8be06f3addbe4cdedce1aa61d1c
JuanDiegoGutierrezDuenas <est.juand.gutierrezd@unimilitar.edu.co>
2026-04-07 10:13:27 -0500

    Se creo el random para que genere numeros aleatorios

--- src/ordenamientoenjava/OrdenamientoEnJava.java
+++ src/ordenamientoenjava/OrdenamientoEnJava.java
@@ -5,12 +5,15 @@ public class OrdenamientoEnJava {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
         System.out.println("¿Cuantos elementos quiere implementar?");
         int n = sc.nextInt();
   
         int arr[] = new int[n];
         
-      
+        for(int i = 0; i<n ; i++){
+            arr[i] = rand.nextInt(201)-100;
+        }
         
         ordenar(arr);
         System.out.println("El arreglo ordenado es: ");
